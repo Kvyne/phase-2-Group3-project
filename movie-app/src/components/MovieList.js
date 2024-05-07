54b13c25ac12c829780966d758332ed7c7a61bf3
Peter <peterkanyi19193@gmail.com>
2024-05-07 21:25:53 +0300

    adds MovieList

--- movie-app/src/components/MovieList.js
+++ movie-app/src/components/MovieList.js
@@ -1,9 +1,16 @@
-import React from 'react'
 
-function MovieList() {
-  return (
-    <div>MovieList</div>
+
+import React from "react";
+
+const MovieList = (props) =>{
+  return(
+    <>
+    {props.movies.map((movie, index)=>
+    <div >
+      <img src={movie.Poster} alt ="movie"></img>
+    </div>
+)}
+    </>
   )
 }
-
-export default MovieList
+ export default MovieList
