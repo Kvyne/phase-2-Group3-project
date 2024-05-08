29d9951f6a06003b0c84edffbac6c9330a22f019
Cindy <cindy.mbishmutisya@gmail.com>
2024-05-08 10:11:19 +0300

    search bar updated

--- movie-app/src/components/SearchBar.js
+++ movie-app/src/components/SearchBar.js
@@ -3,10 +3,12 @@ import React from 'react';
 const SearchBar = (props) => {
     return (
         <div className='search-bar search-bar-sm-4'>
-            
-                <input className="form-control" placeholder="Search...." onChange={props.onChange} />
-                <button type="submit">🔍</button>
-            
+            <input 
+                className="form-control" 
+                placeholder="Search...." 
+                onChange={(event) => props.setSearchValue(event.target.value)} 
+                value={props.value}
+            />
         </div>
     )
 }
