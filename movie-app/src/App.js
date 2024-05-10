f69a4e10db7232d03f74cb0228901ba884df6cb2
Kelvin <frednjogu22@gmail.com>
2024-05-10 23:40:08 +0300

    updated

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
